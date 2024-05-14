61f35387d93a451875f3bd2f968bc6d8a1350609
KURIA FELIX MUHIA <160228937+Kuria-FelixM@users.noreply.github.com>
2024-05-14 22:50:21 +0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -3,7 +3,8 @@ $username = filter_input(INPUT_POST, 'username');
  $password = filter_input(INPUT_POST, 'password');
 if (!empty($username)){
 if (!empty($password)){
-$host = "localhost";
+
+ $host = "localhost";
 $dbusername = "root";
 $dbpassword = "";
 $dbname = "aggregator";
